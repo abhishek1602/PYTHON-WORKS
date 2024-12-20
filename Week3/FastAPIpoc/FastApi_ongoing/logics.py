ba1ecf1cde1361fb4521589939343e1d72290311
abhishek1602 <139642375+abhishek1602@users.noreply.github.com>
2024-12-20 14:55:17 +0530

    Delete Week3 directory

--- Week3/FastAPIpoc/FastApi_ongoing/logics.py
+++
@@ -1,4 +0,0 @@
-class Logic:
-
-    def takeNumber(self, firstNum, secondNum):
-        return int(firstNum)*int(secondNum)
--- Week3/FastAPIpoc/FastApipoc/abcaa.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/abcaa")
-async def root():
-    return {"Hii": "World"}
