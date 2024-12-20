f7b41553c82310941d3175c23d0d8e893085c7cc
Abhishek Manjhi <abhishekmanjhi.1602@gmail.com>
2024-12-20 14:33:37 +0530

    commit

---
+++ Pokemon_API_with_Database/bulk_insert.py
@@ -0,0 +1,34 @@
+import json
+from database import sessionlocal, initialize_db
+from model import Pokemon
+
+def load_json_to_db(json_path : str):
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+
+
+    db = sessionlocal()
+    pokemons = [
+        Pokemon(
+            name=pokemon["name"].lower(),
+            height=pokemon["height"],
+            weight=pokemon["weight"],
+            xp=pokemon["xp"],
+            image_url=pokemon["image_url"],
+            pokemon_url=pokemon["pokemon_url"],
+            abilities=[ability["name"] for ability in pokemon["abilities"]],
+            stats=pokemon["stats"],
+            types=[typ["name"] for typ in pokemon["types"]]
+        )
+        for pokemon in data
+        ]
+
+    db.add_all(pokemons)
+    db.commit()
+    db.close()
+
+if __name__ == "__main__":
+    initialize_db()
+    load_json_to_db("Pokemon_API_with_Database/pokemon_data.json")
+
+    
---
+++ Pokemon_API_with_Database/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Base
+
+DATABASE_URL = "postgresql://postgres:admin1234@localhost/pokemon_database"
+
+engine = create_engine(DATABASE_URL)
+
+sessionlocal = sessionmaker(autocommit=False, bind=engine)
+
+def initialize_db():
+    
+    Base.metadata.create_all(bind=engine)
