20a72b1867c65623b9b2b0b2cfbf24765afb629b
Abhishek Manjhi <abhishekmanjhi.1602@gmail.com>
2025-01-17 17:45:59 +0530

    commit

--- Pokemon_API (Database)/main.py
+++ Pokemon_API (Database)/main.py
@@ -1,11 +1,9 @@
 from router import pokemon_router
-from utils.init_db import initialize_db, load_json_to_db
+from utils.init_db import initialize_db
 from fastapi import FastAPI
 
 app = FastAPI()
 initialize_db()
-load_json_to_db("pokemon_data.json")
-
 app.include_router(pokemon_router.router, prefix="/pokemon", tags=["Pokemon"])
 
 
